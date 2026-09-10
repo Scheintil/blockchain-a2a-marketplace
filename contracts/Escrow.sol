9076dfdd6a49564e197ff09e17d989c4f86b7afd
Moritz-qwertz <moritz.ringel@stud.unibas.ch>
2026-09-10 13:25:47 +0200

    Revert last escrow change

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -28,11 +28,11 @@ contract Escrow {
     /// @param _provider Wallet-Adresse des Providers
     /// @param _deadline Unix-Timestamp, bis zu dem die Mittel maximal gesperrt bleiben
     /// @param _expectedHash keccak256-Hash der erwarteten Loesung ("valid")
-    constructor(address _costumer, address _provider,address _validator, uint256 _deadline, bytes memory _expectedHash) payable {
+    constructor(address _provider,address _validator, uint256 _deadline, bytes memory _expectedHash) payable {
         require(_provider != address(0), "Provider Adresse ungueltig");
         require(_deadline > 0, "Deadline muss in der Zukunft liegen");
         require(_validator != address(0), "Validator Adresse ungueltig");
-        customer = _costumer;
+        customer = msg.sender;
         provider = _provider;
         validator = _validator;
         deadline = _deadline+block.timestamp;
